1247e504ffd5c856482711cbfc07cd3cb3d4a8e8
VISHESH <visheshkumarsharma53@gmail.com>
2024-07-24 19:54:59 +0530

    app.py

--- app.py
+++ app.py
@@ -27,7 +27,11 @@ def translate_to_english(text, src_lang='auto'):
 vishesh_client = InferenceClient("imvishesh007/gemma-Code-Instruct-Finetune-test",token="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")
 madhavi_client = InferenceClient("imvishesh007/gemma-Code-Instruct-Finetune-test",token="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")
 rakesh_client = InferenceClient("bandi333/gemma-Code-Instruct-Finetune-test-v0.0",token="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")
-models = {"vishesh_client": vishesh_client,"rakesh_client": rakesh_client,"madhavi_client": madhavi_client}
+models = {
+    "vishesh_client": vishesh_client,
+    "rakesh_client": rakesh_client,      # Add your token and model for rakesh_client if needed
+    "madhavi_client": madhavi_client      # Add your token and model for madhavi_client if needed
+}
 def process_client(client, df):
     x = ""
     for i in range(df.shape[0]):
